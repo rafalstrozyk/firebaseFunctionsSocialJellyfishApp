adabbdf062bba2aad0de0c428a584cefca7c98f7
rafalstrozyk <rafal.strozyk.biz@gmail.com>
2020-07-14 12:03:38 +0200

    Add get user profile details, getting and commenting on post

--- functions/index.js
+++ functions/index.js
@@ -4,16 +4,29 @@ const app = require('express')();
 
 const FBAuth = require('./util/fbAuth');
 
-const { getAllScreams, postOneScream } = require('./handlers/screams');
-const { signup, login, uploadImage } = require('./handlers/users');
+const { getAllScreams, postOneScream, getScream, commentOnScream } = require('./handlers/screams');
+const {
+	signup,
+	login,
+	uploadImage,
+    addUserDetails,
+    getAuthenticatedUser
+} = require('./handlers/users');
 
 // Scream rout's
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
+app.get('/scream/:screamId', getScream);
+// TODO: delete scream
+// TODO: like a scream 
+// TODO: unlike a scream 
+app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
 
 // Users rout's
 app.post('/signup', signup);
 app.post('/login', login);
-app.post('/user/image', FBAuth, uploadImage)
+app.post('/user/image', FBAuth, uploadImage);
+app.post('/user', FBAuth, addUserDetails);
+app.get('/user', FBAuth, getAuthenticatedUser)
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
