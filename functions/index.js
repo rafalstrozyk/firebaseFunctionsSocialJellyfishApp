fa884c7cad75b7763ab3b8fb34005c3331e8e16b
rafalstrozyk <rafal.strozyk.biz@gmail.com>
2020-07-15 10:40:54 +0200

    Add notifications

--- functions/index.js
+++ functions/index.js
@@ -4,21 +4,25 @@ const app = require('express')();
 
 const FBAuth = require('./util/fbAuth');
 
+const { db } = require('./util/admin');
+
 const {
 	getAllScreams,
 	postOneScream,
 	getScream,
 	commentOnScream,
 	likeScream,
-    unlikeScream,
-    deleteScream
+	unlikeScream,
+	deleteScream
 } = require('./handlers/screams');
 const {
 	signup,
 	login,
 	uploadImage,
 	addUserDetails,
-	getAuthenticatedUser
+	getAuthenticatedUser,
+	getUserDetails,
+	markNotificationsRead
 } = require('./handlers/users');
 const fbAuth = require('./util/fbAuth');
 
@@ -26,7 +30,7 @@ const fbAuth = require('./util/fbAuth');
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
 app.get('/scream/:screamId', getScream);
-app.delete('/scream/:screamId', fbAuth, deleteScream)
+app.delete('/scream/:screamId', fbAuth, deleteScream);
 app.get('/scream/:screamId/like', FBAuth, likeScream);
 app.get('/scream/:screamId/unlike', FBAuth, unlikeScream);
 app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
@@ -37,5 +41,76 @@ app.post('/login', login);
 app.post('/user/image', FBAuth, uploadImage);
 app.post('/user', FBAuth, addUserDetails);
 app.get('/user', FBAuth, getAuthenticatedUser);
+app.get('/user/:handle', getUserDetails);
+app.post('/notifications',FBAuth, markNotificationsRead)
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
+
+exports.createNotificationOnLike = functions
+	.region('europe-west1')
+	.firestore.document('likes/{id}')
+	.onCreate((snapshot) => {
+		db.doc(`/screams/${snapshot.data().screamId}`)
+			.get()
+			.then((doc) => {
+				if (doc.exists) {
+					return db.doc(`/notifications/${snapshot.id}`).set({
+						createdAt: new Date().toISOString(),
+						recipient: doc.data().userHandle,
+						sender: snapshot.data().userHandle,
+						type: 'like',
+						read: false,
+						screamId: doc.id
+					});
+				}
+			})
+			.then(() => {
+				return;
+			})
+			.catch((err) => {
+				console.error(err);
+				return;
+			});
+	});
+
+exports.deleteNotificationOnUnLike = functions
+	.region('europe-west1')
+	.firestore.document('likes/{id}')
+	.onDelete((snapshot) => {
+		db.doc(`/notifications/${snapshot.id}`)
+			.delete()
+			.then(() => {
+				return;
+			})
+			.catch((err) => {
+				console.error(err);
+				return;
+			});
+	});
+
+exports.createNotificationOnComment = functions
+	.region('europe-west1')
+	.firestore.document('comments/{id}')
+	.onCreate((snapshot) => {
+		db.doc(`/screams/${snapshot.data().screamId}`)
+			.get()
+			.then((doc) => {
+				if (doc.exists) {
+					return db.doc(`/notifications/${snapshot.id}`).set({
+						createdAt: new Date().toISOString(),
+						recipient: doc.data().userHandle,
+						sender: snapshot.data().userHandle,
+						type: 'comment',
+						read: false,
+						screamId: doc.id
+					});
+				}
+			})
+			.then(() => {
+				return;
+			})
+			.catch((err) => {
+				console.error(err);
+				return;
+			});
+	});
--- functions/util/admin.js
+++ functions/util/admin.js
@@ -1,7 +1,7 @@
 const admin = require('firebase-admin');
 require('dotenv').config();
 
-const serviceAccount = require('../../key/keyApp.json');
+const serviceAccount = require('../key/keyApp.json');
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount),
 	databaseURL: process.env.REACT_APP_BASEURL,
