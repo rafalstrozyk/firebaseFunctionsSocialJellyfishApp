e710bcd0081f0847eddfb0a971eaf86a78f1327a
rafalstrozyk <rafal.strozyk.biz@gmail.com>
2020-07-15 14:26:11 +0200

    Add onScreamDelete, image

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -1,5 +1,4 @@
 const { db } = require('../util/admin');
-const { info } = require('firebase-functions/lib/logger');
 
 exports.getAllScreams = (req, res) => {
 	db.collection('screams')
@@ -13,7 +12,10 @@ exports.getAllScreams = (req, res) => {
 						screamId: doc.id,
 						body: doc.data().body,
 						userHandle: doc.data().userHandle,
-						createdAt: doc.data().createdAt
+						createdAt: doc.data().createdAt,
+						commentCount: doc.data().commentCount,
+						likeCount: doc.data().likeCount,
+						userImage: doc.data().userImage
 					});
 				});
 				return res.json(screams);
@@ -82,7 +84,7 @@ exports.getScream = (req, res) => {
 // Comment on a comment
 exports.commentOnScream = (req, res) => {
 	if (req.body.body.trim() === '')
-		return res.status(400).json({ error: 'Must not be empty' });
+		return res.status(400).json({ comment: 'Must not be empty' });
 
 	const newComment = {
 		body: req.body.body,
